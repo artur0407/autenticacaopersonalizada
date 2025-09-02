88ec85e2daa5e11e0e798bb5f6a95b08b6ad6a87
Artur <arturpztlopes@gmail.com>
2025-09-02 11:43:25 -0300

    tests routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    DB::connection()->getPdo();
+    echo 'Home';
 });
 
-Route::view('/teste', 'teste');
+Route::view('/teste', 'teste')->middleware('auth');
+
+Route::get('/login', function(){
+    echo 'form de login';
+})->name('login');
+
+Route::middleware('guest')->group(function() {
+    Route::get('/register', function(){
+        echo 'form register';
+    })->name('register');
+});
+
+Route::get('/register', function() {
+    echo 'form register';
+})->name('register')->middleware('guest');
